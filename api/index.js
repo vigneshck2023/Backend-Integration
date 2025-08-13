7b0563540e396d98d9355d23bea3026ec6a37679
Vignesh C K <vigneshck2003.code@gmail.com>
2025-08-13 14:25:19 +0530

    changes in the code

---
+++ api/index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const serverless = require("serverless-http");
+const { initializeDatabase } = require("../db/db.connect");
+const Movie = require("../models/movie.models");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({ origin: "*", credentials: true, optionSuccessStatus: 200 }));
+
+initializeDatabase();
+
+async function readAllMovies() {
+    return await Movie.find({});
+}
+async function readMovieByDirector(directorName) {
+    return await Movie.find({ director: directorName });
+}
+async function readMovieByGenre(genreName) {
+    return await Movie.find({ genre: genreName });
+}
+async function readMovieByTitle(title) {
+    return await Movie.findOne({ title });
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello From Express");
+});
+
+app.get("/movies", async (req, res) => {
+    const movies = await readAllMovies();
+    movies.length ? res.json(movies) : res.status(404).json({ error: "No Movies Found" });
+});
+
+app.get("/movies/director/:directorName", async (req, res) => {
+    const movies = await readMovieByDirector(req.params.directorName);
+    movies.length ? res.json(movies) : res.status(404).json({ error: "No movies found" });
+});
+
+app.get("/movies/genres/:genreName", async (req, res) => {
+    const movies = await readMovieByGenre(req.params.genreName);
+    movies.length ? res.json(movies) : res.status(404).json({ error: "No movies found." });
+});
+
+app.get("/movies/:title", async (req, res) => {
+    const movie = await readMovieByTitle(req.params.title);
+    movie ? res.json(movie) : res.status(404).json({ error: "Movie Not Found" });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
